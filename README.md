4a042e515ff3d09cd34c41f4e535c529620a2909
Ricardo Ramirez Hintze <ramirezhintze@gmail.com>
2018-11-27 11:25:48 -0600

    Project Setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Health Dashboard - UI Practice Project
+
+Original Design by Walter J. Kovalcs.
+https://dribbble.com/shots/5603624-health-dashboard
+
+
+### Version:
+
+1.0.0
+
+### Credits
+
+All design credits go to Walter J. Kovalcs. This project is for practice purposes only.
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+// Compile Sass
+gulp.task('sass', function(){
+  return gulp.src(['src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream())
+});
+
+// Watch & Serve
+gulp.task('serve', ['sass'], function(){
+  browserSync.init({
+    server: './src'
+  });
+
+  gulp.watch(['src/scss/*.scss'], ['sass']);
+  gulp.watch(['src/*.html']).on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
